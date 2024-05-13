bda15ff0beb42a62f781079a893d8c8f2421c0b8
h0namdev <148035274+h0namdev@users.noreply.github.com>
2024-05-13 16:50:44 +0700

    Update UserController.java
    Add RequestParam and PathVariable

--- src/main/java/com/example/lab2/controller/UserController.java
+++ src/main/java/com/example/lab2/controller/UserController.java
@@ -1,7 +1,24 @@
 package com.example.lab2.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+  @GetMapping("/getInfomation")
+    public ResponseEntity<?> doGetInfomation(@RequestParam ("Ten")String name,
+                                        @RequestParam ("Tuoi")String age) {
+        Map<String, String> result = new HashMap<>();
+        result.put("Ten", name);
+        result.put("Tuoi", age);
+        result.put("Lop", "Java5");
+        return ResponseEntity.ok(result);
+    }
+    @GetMapping("/getVariableWithPathVariable/{name}")
+    public ResponseEntity<?> doGetVariableWithPathVariable(@PathVariable String name){
+        return ResponseEntity.ok(name);
+    }
 }
